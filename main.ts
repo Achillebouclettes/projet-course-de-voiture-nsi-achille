acec201adc0b7695f7b17ef43119edac3addc535
Achillebouclettes <vergnonachille@gmail.com>
2025-06-04 08:37:30 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-Maqueen_V5.I2CInit()
 basic.forever(function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 100)
+    if (maqueen.Ultrasonic() > 5) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 100)
+    }
 })
