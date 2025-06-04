a103e426e4c427debede0f091dbe36cb3b8117fb
Achillebouclettes <vergnonachille@gmail.com>
2025-06-04 09:07:24 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
+basic.showIcon(IconNames.Pitchfork)
 basic.forever(function () {
-    if (maqueen.Ultrasonic() > 5) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 100)
+    if (Maqueen_V5.Ultrasonic() < 15) {
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CCW, 100)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, 100)
+    } else {
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 245)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, 255)
     }
 })
